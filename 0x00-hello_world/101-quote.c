c01a1d66e9fc8d990c45d60ee3c18721cdf3a42a
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-07 09:53:11 -0700

    messsage without printf ands puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * mainfonction
+ * return 1
+ *
+ */
+
+int main(void)
+{
+	char chaine[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	write(1, chaine, sizeof(chaine) - 1);
+	return (1);
+}
