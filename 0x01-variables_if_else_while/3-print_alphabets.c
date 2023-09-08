ee9e3223920330dbaf75e250624ea40c3127f2a0
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-08 14:10:20 -0700

    print amlphabet commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+        char car;
+
+        for (car = 'a' ; car <= 'z' ; car++)
+        {
+                putchar (car);
+        }
+	for (car = 'A' ; car <= 'Z' ; car++)
+	{
+		putchar (car);
+	}
+        putchar ('\n');
+        return (0);
+}
