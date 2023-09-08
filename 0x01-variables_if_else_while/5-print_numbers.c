0106a58076b689c44decc70d1ea838adf4284af7
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-08 15:22:30 -0700

    print amlphabet commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int car;
+	
+	for (car = '0' ; car <= '9'  ; car++)
+	{
+		putchar (car);
+	}
+	putchar ('\n');
+	return (0);
+}
