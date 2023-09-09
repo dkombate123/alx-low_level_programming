770617c5fc68f41bb159793ba0dcd74a41141969
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-09 02:12:42 -0700

    My commit for print comb 3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,29 +9,25 @@
  */
 int main(void)
 {
-    int car, car1, car2;
+	int car, car1, car2;
 
-    for (car = 0; car <= 7; car++)
-    {
-        for (car1 = car + 1; car1 <= 8; car1++)
-        {
-             for (car2 = car1 + 1; car2 <= 9; car2++)
-             {
-                 putchar(car + '0');
-                 putchar(car1 + '0');
-                 putchar(car2 + '0');
-
-                 if (car != 7 || car1 != 8 || car2 != 9)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-             }
-
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+	for (car = 0; car <= 7; car++)
+	{
+		for (car1 = car + 1; car1 <= 8; car1++)
+		{
+			for (car2 = car1 + 1; car2 <= 9; car2++)
+			{
+				putchar(car + '0');
+				putchar(car1 + '0');
+				putchar(car2 + '0');
+				if (car != 7 || car1 != 8 || car2 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
