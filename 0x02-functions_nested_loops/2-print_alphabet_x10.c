7d718e89e4b0776890b857c70ec174789d231e93
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-12 14:17:10 -0700

    alphabet then commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+		putchar('\n');
+	}
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -10,6 +10,7 @@
 int main()
 {
 	print_alphabet();
+	print_alphabet_x10();
 	return (0);
 
 }
