2fbf519b792b1c051cb9c2be5d00429cc11069e8
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-12 14:59:54 -0700

    Isalpha commit here

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -9,8 +9,17 @@
 
 int main()
 {
+	int r;
+
 	print_alphabet();
 	print_alphabet_x10();
+	 r = _islower('H');
+    putchar(r + '0');
+    r = _islower('o');
+    putchar(r + '0');
+    r = _islower(108);
+    putchar(r + '0');
+    putchar('\n');
 	return (0);
 
 }
