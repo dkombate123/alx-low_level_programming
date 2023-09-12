789a984bc434b8acff903afacd65f286a7fc2e69
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-12 13:54:04 -0700

    My commit alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
+/**
+ * print_alphabet - fonction that print lowercase alphabet
+ *
+ * 
+ */
 void print_alphabet(void)
 {
-	char c ;
+	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
