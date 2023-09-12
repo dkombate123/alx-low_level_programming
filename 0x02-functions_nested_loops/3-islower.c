1144a66b003fe2fa84e64217204c992b48565ce8
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-12 15:07:15 -0700

    Isalpha commit here

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+/**
+ * print_alphabet_x10 - Entry point of the program
+ *
+ * Return: 1 (Success)
+ */
+int _islower(int c)
+{
+	if(islower(c))
+		return (1);
+	else
+		return (0);
+}
