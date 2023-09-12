09d22eb1a759f10d8d579501a3ee30312fc3c550
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-12 11:39:53 -0700

    My commit for print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+
+
+int main()
+{
+	char chaine[]="_putchar";
+	int i;
+
+	for(i=0; chaine[i]!='\0';i++)
+	{
+		putchar(chaine[i]);
+	}
+	putchar('\n');
+	return (0);
+
+
+}
+
+
