81ba2bc7bb201756d04e433430c85e1764c1d71c
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-13 12:42:28 -0700

    MY ccommit loops

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -7,23 +7,33 @@
  */
 int main(void)
 {
-	int c;
-	
-	print_alphabet();
-	print_alphabet_x10();
-	c = _islower('T');
-	putchar(c + '0');
-	r = _islower('a');
-	putchar(c + '0');
-	r = _islower(208);
-	putchar(c + '0');
-	c = _islower('X');
+        int c;
+
+        print_alphabet();
+        print_alphabet_x10();
+        c = _islower('o');
         putchar(c + '0');
-        r = _islower('B');
+        c = _islower('108');
         putchar(c + '0');
-        r = _islower('p');
+        c = _islower('c');
         putchar(c + '0');
-	putchar('\n');
-	return (0);
+        c = _islower('I');
+        putchar(c + '0');
+        c = _islower('s');
+        putchar(c + '0');
+        c = _islower('70');
+        putchar(c + '0');
+         c = _islower('20');
+        putchar(c + '0');
+        c = _islower('U');
+        putchar(c + '0');
+        c = _islower('n');
+         putchar(c + '0');
+         c = _islower(0);
+        putchar(c + '0');
+        c = _islower('H');
+        putchar(c + '0');
+        putchar('\n');
+        return (0);
 
 }
