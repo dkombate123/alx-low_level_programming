51dee2423684fe9e54a74ad9991a819f8db4250f
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-13 13:26:06 -0700

    my commit for loops

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,26 @@
+
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+        int c;
+        
+        c = _islower('o');
+        c = _islower('108');
+        c = _islower('c')
+        c = _islower('I');
+        c = _islower('s');
+        c = _islower('70');
+         c = _islower('20');
+        c = _islower('U');
+        c = _islower('n');
+        c = _islower(0);
+        c = _islower('H');
+        return (0);
+
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -7,22 +7,10 @@
  */
 int main(void)
 {
-        int c;
+ 
 
         print_alphabet();
         print_alphabet_x10();
-        c = _islower('o');
-        c = _islower('108');
-        c = _islower('c')
-        c = _islower('I');
-        c = _islower('s');
-        c = _islower('70');
-         c = _islower('20');
-        c = _islower('U');
-        c = _islower('n');
-         c = _islower(0);
-        c = _islower('H');
-        putchar('\n');
         return (0);
 
 }
