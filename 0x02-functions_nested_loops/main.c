2b30e4f0337764e2c90625842696bea375e2eb07
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-13 12:46:21 -0700

    MY ccommit loops

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -12,27 +12,16 @@ int main(void)
         print_alphabet();
         print_alphabet_x10();
         c = _islower('o');
-        putchar(c + '0');
         c = _islower('108');
-        putchar(c + '0');
-        c = _islower('c');
-        putchar(c + '0');
+        c = _islower('c')
         c = _islower('I');
-        putchar(c + '0');
         c = _islower('s');
-        putchar(c + '0');
         c = _islower('70');
-        putchar(c + '0');
          c = _islower('20');
-        putchar(c + '0');
         c = _islower('U');
-        putchar(c + '0');
         c = _islower('n');
-         putchar(c + '0');
          c = _islower(0);
-        putchar(c + '0');
         c = _islower('H');
-        putchar(c + '0');
         putchar('\n');
         return (0);
 
