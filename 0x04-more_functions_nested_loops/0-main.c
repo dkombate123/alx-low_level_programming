d9b1b914cc16d5b530e0388aacafe90ab0db00e3
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-14 15:35:21 +0300

    my first commit

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -3,34 +3,36 @@
 #include <ctype.h>
 
 /**
- *  * main - Entry point of the program
- *   *
- *    * Return: 0 (Success)
- *     */
-
-/**
- *  * print_alphabet_x10 - Entry point of the program
- *   *
- *    * Return: 1 (Success)
- *     */
+ * * print_alphabet_x10 - Entry point of the program
+   *
+     * Return: 1 (Success)
+   */
 
 int main()
 {
 	char c;
        	int r;
 	
-	c='A';
+	c='C';
 	r= _putchar(c);
 	_isupper(r);
 	_putchar(':');
 	_putchar(' ');
 	_putchar(r+'0');
 	_putchar('\n');
-	c='a';
+	c='E';
 	r= _putchar(c);
 	_isupper(r);
 	_putchar(':');
 	_putchar(' ');
+	_putchar(r+'0');
+	_putchar('\n');
+	c='b';
+	r= _putchar(c);
+	_isupper(r);
+	_putchar(':');
+	_putchar(' ');
+	_putchar(r+'0'); 
 	_putchar(r+'0');
 	_putchar('\n');
 	return (0);
