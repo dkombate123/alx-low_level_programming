505a5bdc60fca0141177ebef410202dd2cc0f410
root <root@kombate.localdomain>
2023-09-14 14:40:30 +0300

    My commit for isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * isupper : function to determine if caracter is uppercase
+ *
+ * return : 1 (sucess)
+ */
+int _isupper(int c)
+{
+	if(isupper(c))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *  * main - Entry point of the program
+ *   *
+ *    * Return: 0 (Success)
+ *     */
+
+/**
+ *  * print_alphabet_x10 - Entry point of the program
+ *   *
+ *    * Return: 1 (Success)
+ *     */
+
+int main()
+{
+	int r;
+	
+	r = _isupper('H');
+	_putchar(':');
+	_putchar(r + '0');
+	r = _isupper('o');
+	_putchar(':');
+	_putchar(r + '0');
+	_putchar(':');
+	r = _isupper(108);
+	_putchar(r + '0');
+	return (0);
+
+
+}
