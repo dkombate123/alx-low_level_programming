e9debd09f5db2d8c13458e3e38cd64a0a72e8be0
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-15 04:38:36 +0300

    my commit

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	print_numbers();
+	return (0);
