3b224e21da087af5402553918ed1a80574169412
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-16 21:29:54 +0300

    My commit for abs return

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - function for absolute value
+ * @a: number enter
+ *
+ * Return: abs(a) (Success)
+ */
+int _abs(int a)
+{
+	return (abs(a));
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int c;
+
+	c = _abs(-3);
+	_putchar(c + '0');
+	_putchar('\n');
+	c = _abs(-2);
+	_putchar(c + '0');
+	_putchar('\n');
+	return (0);
+}
