f5e4f7ab184f260ba1d052464bc9c8ba640431e9
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-16 22:14:20 +0300

    My commit for abs return

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -10,10 +10,13 @@ int main(void)
 	int c;
 
 	c = print_last_digit(59);
-	_putchar(c + '0');
+	_putchar('0' + c);
+	_putchar('0' + c);
 	c = print_last_digit(10);
-	_putchar(c + '0');
+	_putchar('0' + c);
+	_putchar('0' + c);
 	c = print_last_digit(-594);
+	_putchar('0' + c);
 	_putchar('0' + c);
 	_putchar('\n');
 	return (0);
