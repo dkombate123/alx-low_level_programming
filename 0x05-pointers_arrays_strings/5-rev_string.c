d6f48f21bc3090f9bf11769e085a9bb18ac5b6e0
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-19 00:50:40 +0300

    My commit for reverse string function

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - function to reverse a string
+ *
+ * @s: pointer string
+ * Return: 0 (Success)
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
