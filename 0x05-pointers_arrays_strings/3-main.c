f7ee0251f2b433a1e0b05767c91266b2884f1f0b
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-19 00:00:56 +0300

    My commit for puts function

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include "main.h"
+#include <string.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int *str = "KOMBATE DANDIEGOU";
+
+	_puts(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - function to print a string
+ *
+ *@str: pointer to string
+ * Return: 1 (Success)
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
