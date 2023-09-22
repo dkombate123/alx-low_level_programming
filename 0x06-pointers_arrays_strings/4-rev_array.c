77358e84adc9ab1a2bee255028ca024c8e87e810
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-22 22:12:32 +0300

    commit for my reverse array function

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include "main.h"
+#include <string.h>
+/**
+ * reverse_array - fonction to reverse array
+ *
+ *@a : var1 array
+ *@n: size of array
+ * Return: no (Success)
+ */
+void reverse_array(int *a, int n)
+{
+	int i, num;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		if (i != n - 1)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		num = a[i];
+		if (num < 0)
+		{
+			_putchar('-');
+			num = -num;
+		}
+
+		int divisor = 1;
+
+		while (num / divisor >= 10)
+		{
+			divisor *= 10;
+		}
+		while (divisor != 0)
+		{
+			int digit = (num / divisor) % 10;
+
+			_putchar('0' + digit);
+			divisor /= 10;
+		}
+	}
+}
