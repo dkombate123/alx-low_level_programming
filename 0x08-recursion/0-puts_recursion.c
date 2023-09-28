3d47a01f1016e38ae51615a1811a515151efd328
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-29 00:41:42 +0300

    Commit for recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _puts_recursion - recursion function
+ * @s: The character to print
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_putchar(s);
+		_puts_recursion(s + 1);
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,6 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 int factorial(int n);
+void _puts_recursion(char *s);
+int _putchar(char *c);
 #endif
