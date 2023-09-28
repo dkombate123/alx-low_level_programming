98451c48719aaa9b9aeec8d8bc409be44361add7
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-09-29 01:14:08 +0300

    Commit for recursion

--- 0x08-recursion/0-main.c
+++ 0x08-recursion/0-main.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 	_puts_recursion("Puts with recursion Kombate Dandiegou");
-	printf("\n");
+	_putchar('\n');
 	return (0);
 }
--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -20,6 +20,5 @@ void _puts_recursion(char *s)
 			_putchar(*s);
 		}
 		_puts_recursion(s + 1);
-		_putchar('\n');
 	}
 }
