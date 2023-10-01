ee1511df7c193586681896b62f9a07683038024e
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-10-02 00:14:22 +0300

    Recursion string length longer function

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_strlen_recursion - recursion fonction
+*@s: string
+*
+*Return: values
+*/
+int  _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
