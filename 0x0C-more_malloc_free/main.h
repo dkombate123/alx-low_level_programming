894c996bbf85e5bb2e8f2e9c7e48ad094124a5ca
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-10-08 23:16:51 +0300

    commit for more malloc and free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,4 +3,5 @@
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
+void simple_print_buffer(int *buffer, unsigned int size);
 #endif
