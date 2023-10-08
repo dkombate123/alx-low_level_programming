3b08188073bf89d47c50aa7d2b7ac8b0ae3f6ecc
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-10-08 15:05:19 +0300

    commit for function to print array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - prints array
+ * @c: the character
+ * @size: the size of the memory to print
+ *
+ * Return: array or NULL.
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	char *tab = (char *)malloc(sizeof(char) * size);
+
+	if (tab == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		tab[i] = c;
+	}
+	return (tab);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,4 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+char *create_array(unsigned int size, char c);
+#endif
