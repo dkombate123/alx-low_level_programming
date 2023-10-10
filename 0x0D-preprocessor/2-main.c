e0dced6aa8634d8562d092e1a31cebd499b671e1
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-10-11 00:23:27 +0300

    commit for macro function in c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,34 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
- *printString - writes the character c to stdout
- * @str: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-void printString(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-}
 
 /**
  * main - check the code
@@ -38,7 +10,6 @@ void printString(char *str)
 
 int main(void)
 {
-	printString(__FILE__);
-	_putchar('\n');
+	printf("%s\n", __FILE__);
 	return (0);
 }
