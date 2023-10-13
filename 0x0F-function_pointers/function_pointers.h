3181f2ce8ca3996d873ad63a58ec66c20aa94311
KOMBATE Dandiegou Mohammad <kdandiegou@gmail.com>
2023-10-13 14:13:11 +0300

    commit for pointer function

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 int _putchar(char c);
+void print_name_uppercase(char *name);
+void print_name_as_is(char *name);
 void print_name(char *name, void (*f)(char *));
 void print_name(char *name, void (*f)(char *));
 #endif
