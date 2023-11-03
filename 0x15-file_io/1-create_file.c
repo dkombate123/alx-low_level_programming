6c1a85f308f7ec0bf4fffb0f96b08f0aed78e045
KOMBATE DANDIEGOU <kdandiegou@gmail.com>
2023-11-03 21:27:59 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,9 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+	/*ssize_t bytes_written = write(fd, text_content, strlen(text_content));*/
+	size_t content_length = strlen(text_content);
+	size_t i;
 
 	if (filename == NULL)
 	{
@@ -26,10 +28,13 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		if (bytes_written == -1)
+		for (i = 0; i < content_length; i++)
 		{
-			close(fd);
-			return (-1);
+			if (write(fd, &text_content[i], 1) == -1)
+			{
+				close(fd);
+				return (-1);
+			}
 		}
 	}
 	close(fd);
