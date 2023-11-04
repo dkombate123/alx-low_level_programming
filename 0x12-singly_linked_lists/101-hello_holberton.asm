51eddb5d3adff9c69f2e2bda2d539d8ba99ef051
KOMBATE DANDIEGOU <kdandiegou@gmail.com>
2023-11-04 22:02:13 +0300

    Create 101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .data
+    hello db "Hello, Holberton",10,0   ; The string to print (10 is the newline character)
+    format db "%s",0                 ; Format specifier for printf (null-terminated)
+
+section .text
+    global main
+
+main:
+    ; Call printf with our string
+    push rdi                ; Preserve RDI (callee-saved register)
+    push rsi                ; Preserve RSI (callee-saved register)
+    lea rdi, [format]       ; Load the format specifier address
+    lea rsi, [hello]        ; Load the string address
+    call printf             ; Call printf
+    pop rsi                 ; Restore RSI
+    pop rdi                 ; Restore RDI
+    ret
