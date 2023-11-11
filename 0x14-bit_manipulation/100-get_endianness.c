08315972247d29eeedd1fa6cdd284221f9b6a65e
KOMBATE DANDIEGOU <kdandiegou@gmail.com>
2023-11-11 12:15:59 +0300

    Create 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * get_endianness - check the code
+ *
+ * Return: Always 0.
+ */
+/**
+ *struct union - singly linked list
+ * @i: integer
+ * @c: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+int get_endianness(void)
+{
+	union
+	{
+		uint32_t i;
+		uint8_t c[4];
+	}
+	check;
+	check.i = 1;
+	return ((check.c[0] == 1) ? 1 : 0);
+}
