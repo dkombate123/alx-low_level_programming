8b110ad78b83a7b5043266f5adfaace75d90615e
KOMBATE DANDIEGOU <kdandiegou@gmail.com>
2023-11-11 13:39:51 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,6 +12,8 @@
 int **alloc_grid(int width, int height)
 {
 	int **grid = (int **)malloc(height * sizeof(int *));
+	int j;
+	int i;
 
 	if (width <= 0 || height <= 0)
 	{
@@ -21,19 +23,19 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	for (int i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			for (int j = 0; j < i; j++)
+			for (j = 0; j < i; j++)
 			{
 				free(grid[j]);
 			}
 			free(grid);
 			return (NULL);
 		}
-		for (int j = 0; j < width; j++)
+		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
 		}
