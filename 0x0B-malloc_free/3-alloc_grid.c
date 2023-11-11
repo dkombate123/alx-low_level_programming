f7503896fec3ef52f9dbd5fc061b628f0a1e22c8
KOMBATE DANDIEGOU <kdandiegou@gmail.com>
2023-11-11 13:36:06 +0300

    Create 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * alloc_grid - check the code
+ *@width:var
+ *@height:vare
+ *
+ * Return: Always 0.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid = (int **)malloc(height * sizeof(int *));
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (int i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (int j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
