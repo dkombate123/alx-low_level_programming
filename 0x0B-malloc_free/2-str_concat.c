fb55bf7addf03a4b79f894a876baba015b07fcad
KOMBATE DANDIEGOU <kdandiegou@gmail.com>
2023-11-11 13:21:23 +0300

    Create 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * str_concat - check the code
+ *@s1:var
+ *@s2:vare
+ *
+ * Return: Always 0.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *result = (char *)malloc((len_s1 + len_s2 + 1) * sizeof(char));
+	size_t len_s1 = strlen(s1);
+	size_t len_s2 = strlen(s2);
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(result, s1);
+	strcat(result, s2);
+	return (result);
+}
+
